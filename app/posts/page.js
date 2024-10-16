d7edc305deb097c4c7557d678bd79766e3ad3884
Göksel Ağan <gokselagan@outlook.com>
2024-10-16 21:24:45 +0300

    first commit necessary pages added

--- app/posts/page.js
+++ app/posts/page.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const page = () => {
   return (
+<<<<<<< HEAD
     <div>Posts</div>
+=======
+    <div>POSTS</div>
+>>>>>>> a3bdbd1 (first commit necessary pages added)
   )
 }
 
